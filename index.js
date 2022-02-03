99906c4e29e55f77de981e19a0e99562b53bcd2e
tylergubser <tylergubser2200@gmail.com>
2022-02-03 12:20:06 -0600

    commit tree cece7090eac15f1417c89ec0386ac096c9460f6e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName () {
+     customerName =  customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+     
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const LeastFavoriteCustomer = 3;
+function changeLeastFavoriteCustomer() {
+    return LeastFavoriteCustomer = 2;
+}
